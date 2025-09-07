a8955670d6bfd127a3d9dde0db7b007587f9f7f4
asyncsage <sauravj271@gmail.com>
2025-09-07 11:39:28 +0530

    added header and theme css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,27 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@astrojs/react'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import robotsTxt from 'astro-robots-txt'
+import expressiveCode from 'astro-expressive-code'
+import { remarkPlugins, rehypePlugins } from './plugins'
+// import { SITE } from './src/config'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+//   site: SITE.website,
+//   base: SITE.base,
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'viewport',
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  markdown:  {
+    syntaxHighlight: false,
+    remarkPlugins,
+    rehypePlugins,
+  },
+  integrations: [expressiveCode(), mdx(), react(), sitemap(), robotsTxt()],
+})
